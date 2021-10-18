0ca9b38ac375a67dd2a8c1df9d6d117f0951c9f7
aldyoka <aldyoka45@gmail.com>
2021-10-18 14:12:41 +0800

    memperbaiki small dan medium view, bg small maih belum fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,9 @@
-function customToggle(){
+function customToggleDrop(){
     const toggleCustom = document.querySelector('.menu_custom');
     toggleCustom.classList.toggle('active')
 }
 
+function customToggleSide(){
+    const toggleCustom = document.querySelector('.menu_side_custom');
+    toggleCustom.classList.toggle('active')
+}
