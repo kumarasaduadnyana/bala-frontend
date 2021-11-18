803cc187f418d8c8bb3d774b8f6098f1b4b86f92
aldyoka <aldyoka45@gmail.com>
2021-11-19 06:44:43 +0800

    fixing detail pesanan

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,3 +7,17 @@ function customToggleSide(){
     const toggleCustom = document.querySelector('.menu_side_custom');
     toggleCustom.classList.toggle('active')
 }
+
+// detail counter
+var count = 1;
+var countEl = document.getElementById("count");
+function plus(){
+    count++;
+    countEl.value = count;
+}
+function minus(){
+  if (count > 1) {
+    count--;
+    countEl.value = count;
+  }  
+}
